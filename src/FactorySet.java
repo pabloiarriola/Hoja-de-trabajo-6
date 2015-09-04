ee965cab65e61ceec9c1e3cebedced1b58346639
Pablo Arriola <pabloiad@hotmail.com>
2015-09-03 18:38:47 -0600

    Hoja de trabajo 6

---
+++ src/FactorySet.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+/**
+ *
+ * @author Pablo Arriola
+ * @author Julio Dieguez
+ */
+public class FactorySet {
+    Set setEsperar;
+
+    public FactorySet() {
+    }
+    public Set getSet(int tipoSet){
+       if (tipoSet ==1 ){
+           // crear un HashSet
+           setEsperar= new HashSet<String>();
+       } 
+       else if (tipoSet==2){
+           // crear un TreeSet
+           setEsperar = new TreeSet<String>();
+       }
+       else{
+           //se crea un LinketSet
+           setEsperar = new LinkedHashSet<String>();
+        
+       }
+       //retorna el set solicitado
+       return setEsperar;
+    }
+}
---
+++ src/HojaDeTrabajo6.java
@@ -0,0 +1,17 @@
+/**
+ * @author Pablo Arriola
+ * @author Julio Dieguez
+ */
+public class HojaDeTrabajo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     Acciones nuevo = new Acciones();
+        
+        
+    }
+    
+}
